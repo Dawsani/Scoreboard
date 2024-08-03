6447b9c8ca37b545c08e41b4a9d9aa2ea90404fe
dawson <dawson.dwm@gmail.con>
2024-08-03 14:20:42 -0700

    added 4 player win rate

--- archived/scoreboard.php
+++ archived/scoreboard.php
@@ -4,13 +4,6 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-
-<style>
-table, th, td {
-  border:1px solid black;
-}
-</style>
-
 <title>
 <?php
 	$scoreboardId = $_GET['scoreboardId'];
@@ -87,75 +80,46 @@ if ($recentGames->num_rows > 0) {
 }
 ?>
 
-<h2>4 Player Win Rates</h2>
+<h2>Games Won</h2>
 
 <?php
-
-$sql = "SET @numPlayers := 4;";
-$conn->query($sql);
-
 // Show each players games played by number of players in the game
 $sql = "
-SELECT games_played.player_name, COALESCE(games_won, 0) AS games_won, games_played, COALESCE(games_won, 0) / games_played * 100 AS win_rate
-FROM
-(
-SELECT player.name AS player_name, COUNT(*) AS games_played
-FROM game_entry
-JOIN (
-	SELECT game_id, COUNT(*) AS num_players
-	FROM game
-	JOIN game_entry ON game.id = game_entry.game_id
-	GROUP BY game_id
-) AS game_players
-ON game_entry.game_id = game_players.game_id
-JOIN player ON player.id = game_entry.player_id
-WHERE num_players = @numPlayers
-GROUP BY player_name
-) AS games_played
-
-LEFT JOIN
-
-(
-SELECT player.name AS player_name, COUNT(*) AS games_won
-FROM game
-JOIN (
-	SELECT game_id, COUNT(*) AS num_players
-	FROM game
-	JOIN game_entry ON game.id = game_entry.game_id
-	GROUP BY game_id
-) AS game_players
-ON game.id = game_players.game_id
-JOIN player ON player.id = game.winner_id
-WHERE num_players = @numPlayers
-GROUP BY player_name
-
-) AS games_won
-ON games_played.player_name = games_won.player_name;";
+	SELECT player.name AS player_name, num_players, COUNT(*) AS games_played
+	FROM (
+	        SELECT game_id, COUNT(*) AS num_players
+	        FROM game
+	        JOIN game_entry ON game.id = game_entry.game_id
+	        GROUP BY game_id
+	) AS game_num_players
+	JOIN game_entry ON game_num_players.game_id = game_entry.game_id
+	JOIN player ON player.id = game_entry.player_id
+	GROUP BY player_name, num_players
+	ORDER BY player_name, num_players;";
 
 $result = $conn->query($sql);
-
 if ($result->num_rows == 0) {
-	echo "No existing data.<br>";
+	echo "No data exists.<br>";
 	exit();
 }
 
-echo "<table>
-	<tr>
-	<th>Player</th>	<th>Win Rate</th>
-	</tr>";	
 
 while ($row = $result->fetch_assoc()) {
+	$players = array();
+
 	$playerName = $row['player_name'];
-	$gamesPlayed = $row['games_played'];
-	$gamesWon = $row['games_won'];
-	$winRate = $row['win_rate'];
+	$numPlayers = $row['num_players'];
+	$gamesPlayed = $row['gamesPlayed'];
 
-	echo "<tr>
-		<td>$playerName</td><td>$gamesWon/$gamesPlayed $winRate%</td>
-		</tr>";
-}
+	// If this is a new player, make a new column
+	if (!in_array($playerName, $players)) {
+		array_push($players, array(gamesPlayed));
+		
+	}
+	// otherwis
+	else {
 
-echo "</table>";
+
 
 ?>
 </body>
--- sql_scripts/stats.sql
+++ sql_scripts/stats.sql
@@ -41,3 +41,69 @@ JOIN player ON player.id = game_entry.player_id
 WHERE num_players = 4
 GROUP BY player_name
 ORDER BY player_name;
+
+-- Games won with X players
+SET @numPlayers := 4;
+SELECT player.name AS player_name, COUNT(*) AS games_won
+FROM game
+JOIN (
+	SELECT game_id, COUNT(*) AS num_players
+	FROM game
+	JOIN game_entry ON game.id = game_entry.game_id
+	GROUP BY game_id
+) AS game_players
+ON game.id = game_players.game_id
+JOIN player ON player.id = game.winner_id
+WHERE num_players = @numPlayers
+GROUP BY player_name;
+
+-- Get games played by player and by number of players
+SELECT player.name AS player_name, num_players, COUNT(*) AS games_played  
+FROM (
+	SELECT game_id, COUNT(*) AS num_players
+	FROM game
+	JOIN game_entry ON game.id = game_entry.game_id
+	GROUP BY game_id
+) AS game_num_players
+JOIN game_entry ON game_num_players.game_id = game_entry.game_id
+JOIN player ON player.id = game_entry.player_id
+GROUP BY player_name, num_players
+ORDER BY player_name, num_players;
+
+-- Get winrate by player for games with X players
+SET @numPlayers := 3;
+SELECT games_played.player_name, COALESCE(games_won, 0) AS games_won, games_played, COALESCE(games_won, 0) / games_played * 100 AS win_rate
+FROM
+(
+SELECT player.name AS player_name, COUNT(*) AS games_played
+FROM game_entry
+JOIN (
+	SELECT game_id, COUNT(*) AS num_players
+	FROM game
+	JOIN game_entry ON game.id = game_entry.game_id
+	GROUP BY game_id
+) AS game_players
+ON game_entry.game_id = game_players.game_id
+JOIN player ON player.id = game_entry.player_id
+WHERE num_players = @numPlayers
+GROUP BY player_name
+) AS games_played
+
+LEFT JOIN
+
+(
+SELECT player.name AS player_name, COUNT(*) AS games_won
+FROM game
+JOIN (
+	SELECT game_id, COUNT(*) AS num_players
+	FROM game
+	JOIN game_entry ON game.id = game_entry.game_id
+	GROUP BY game_id
+) AS game_players
+ON game.id = game_players.game_id
+JOIN player ON player.id = game.winner_id
+WHERE num_players = @numPlayers
+GROUP BY player_name
+
+) AS games_won
+ON games_played.player_name = games_won.player_name;
